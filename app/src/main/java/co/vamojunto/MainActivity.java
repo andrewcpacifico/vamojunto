b364dcf902809bb2cf7b94d2101c95e3705d0282
Andrew Pacífico <andrewcpacifico@gmail.coM>
2015-02-09 23:46:59 -0400

    Exibe uma janela de diálogo solicitando que o usuário ative o GPS, caso o mesmo esteja desativado no aparelho.

--- app/src/main/java/co/vamojunto/MainActivity.java
+++ app/src/main/java/co/vamojunto/MainActivity.java
@@ -11,18 +11,18 @@
 package co.vamojunto;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
-import android.provider.SyncStateContract;
-import android.support.v7.app.ActionBarActivity;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -32,12 +32,10 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.parse.ParseUser;
 
-import java.text.BreakIterator;
-
 /**
  * Activity principal do sistema.
  *
- * @author Andrew C. Pacifico (andrewcpacifico@gmail.com)
+ * @author Andrew C. Pacifico <andrewcpacifico@gmail.com>
  * @since 0.1.0
  */
 public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks,
@@ -46,11 +44,23 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
     /** Usado para identificação nos logs */
     private static final String TAG = "MainActivity";
 
+    /** Guarda a instância do MapFragment da Activity */
     private GoogleMap mMap;
+
+    /** Instância da Google Play Services API Client, utilizada para manipular o Location API */
     private GoogleApiClient mGoogleApiClient;
+
+    /** Armazena a última localização do usuário */
     private Location mLastLocation;
-    private BreakIterator mLatitudeText;
-    private BreakIterator mLongitudeText;
+
+    /** Booleano para verificar se o app está resolvendo algum erro. */
+    private boolean mResolvingError = false;
+
+    /**
+     * Flag que indica se é o primeiro start da Activity, utilizada para inicializar a localização
+     * do mapa na localização atual do usuário.
+     */
+    private boolean mFirstStart = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,14 +78,66 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
             this.finish();
         }
 
+        // Verifica se os serviços de localização estão ativados no aparelho. Caso não estejam,
+        // exibe um diálogo para o usuário solicitando que ele ative.
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+
+        // Constrói o GoogleApiClient
         buildGoogleApiClient();
+
+        // Inicializa as configurações do MapFragment
         initMap();
-        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    /**
+     * Exibe um diálogo de alerta para o usuário, indicando que ele deve ativar os serviços de
+     * localização no aparelho. O diálogo redireciona para a tela de configuração de localização
+     * caso o usuário pressione "Sim", ou não faz nada se o usuário pressionar o botão "Não".
+     */
+    private  void buildAlertMessageNoGps() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.no_gps_dialog_text))
+                .setTitle(getString(R.string.no_gps_dialog_title));
+
+        builder.setPositiveButton(getString(R.string.no_gps_dialog_button_yes), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Muda o status da flag, para que o mapa seja reposicionado quando o usuário,
+                // retornar à aplicação.
+                mFirstStart = true;
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        });
+
+        builder.setNegativeButton(getString(R.string.no_gps_dialog_button_no), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     /**
      * Inicializa as configurações do mapa. Instancia o objeto GoogleMap a partir do Fragment,
-     * então posiciona o mapa na posição atual do usuário.
      */
     protected void initMap() {
         if (mMap == null) {
@@ -85,6 +147,22 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
         mMap.setMyLocationEnabled(true);
     }
 
+    /**
+     * Inicializa a localização e o zoom do mapa. Posiciona na localização atual do usuário.
+     */
+    protected void initMapLocation() {
+        // Posiciona o mapa na posição atual do usuário, e dá um zoom de 17.0
+        if (mLastLocation != null) {
+            LatLng coord = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 17.0f));
+        } else {
+            Log.e(TAG, "mLastLocation ainda não foi inicializado.");
+        }
+    }
+
+    /**
+     * Inicializa o campo que armazenará a instância da GoogleApiClient
+     */
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -93,15 +171,24 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
                 .build();
     }
 
+    /**
+     * Executado quando a conexão da GoogleApiClient é finalizada. Obtém a localização atual do
+     * usuário, e armazena no campo mLastLocation.
+     * @param bundle
+     */
     @Override
     public void onConnected(Bundle bundle) {
         Log.i(TAG, "onConnected");
 
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-        if (mLastLocation != null) {
-            LatLng coord = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 16.0f));
+
+        // Posiciona o mapa na localização atual do usuário, apenas se for a primeira vez que o
+        // evento está sendo executado, ou caso o usuário tenha utilizado o dialog exibido para
+        // ativar o serviço de localização no aparelho.
+        if (mFirstStart) {
+            initMapLocation();
+            mFirstStart = false;
         }
     }
 
@@ -112,7 +199,7 @@ public class MainActivity extends Activity implements GoogleApiClient.Connection
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i(TAG, "onConnectionFaild");
+        Log.i(TAG, "onConnectionFailed");
     }
 
     @Override
